267907875e21b1de9e641de519464054f989794a
吴鑫磊 <goldbridge_wuxl55@126.com>
2015-06-24 18:29:31 +0800

    提交 gitignore测试一下

---
+++ github测试2/github测试2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github测试2
+//
+//  Created by wuxl on 15/6/24.
+//  Copyright © 2015年 heima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
