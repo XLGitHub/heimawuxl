7efeb882d132be249fde28a3c4a382a90872ad5a
吴鑫磊 <goldbridge_wuxl55@126.com>
2015-06-24 18:07:01 +0800

    就是让我们来happy一下吧

--- gitDemo/gitDemo/ViewController.swift
+++ gitDemo/gitDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("让我们来happy一下吧")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
