c22650eff64da1475c8ccf71023f5f4925b131fd
吴鑫磊 <goldbridge_wuxl55@126.com>
2015-06-24 18:47:26 +0800

    777

--- github测试2/github测试2/ViewController.swift
+++ github测试2/github测试2/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        // 这是什么东东 来测一下 git log
+        // 这是什么东东 来测一下 git 
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
